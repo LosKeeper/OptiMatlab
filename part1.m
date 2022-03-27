cd68822e5254c54c3d8643468d7c03a36c0da8cf
unknown <thomas.dumond2001@gmail.com>
2022-03-27 18:53:23 +0200

    bourrin

--- part1.m
+++ part1.m
@@ -1,19 +1,24 @@
-function [f,x,B] = LP_bourrin(A,b,c)
-    if  ~ismatrix(A) | ~ismatrix(b) | ~ismatrix(c)
+function [f, x, B] = LP_bourrin(A, b, c)
+
+    if ~ismatrix(A) || ~ismatrix(b) || ~ismatrix(c)
         error('Not matrix or vector');
     end
 
-    [m,n]=size(A);
+    [m, n] = size(A);
 
-    if rank(A)<m
+    if rank(A) < m
         error('A not full rank');
     end
 
-    if length(b) ~=m | length(c) ~=n
+    if length(b) ~= m || length(c) ~= n
         error('b and c dont have same dimensions as A');
     end
 
-    for i=1:m
-        D = nchoosek(A(i,1:n),m)
-
-end
+    for r1 = 1:m
+        for r2 = 1:n
+            B = [A(r1,1:n) A(r2,1:n)];
+            Binv = inv(B);
+            xb=Binv*b;
+            
+        end
+    end
--- part2_1.m
+++ part2_1.m
@@ -4,26 +4,30 @@ clear all;
 ro = 1/3;
 a0 = -2;
 b0 = 2;
-a1 = ro*(b0-a0)+a0;
-b1 = b0 - ro*(b0-a0);
+a1 = ro * (b0 - a0) + a0;
+b1 = b0 - ro * (b0 - a0);
 eps = 10^(-2);
-i=0;
+i = 0;
 
-while abs(b0-a0)>eps
-    i=i+1;
-    if f(a1)<=f(b1)
-        b0=b1;
+while abs(b0 - a0) > eps
+    i = i + 1;
+
+    if f(a1) <= f(b1)
+        b0 = b1;
     end
-    if f(a1)>f(b1)
-        a0=a1;
+
+    if f(a1) > f(b1)
+        a0 = a1;
     end
-    a1 = ro*(b0-a0)+a0;
-    b1 = b0 - ro*(b0-a0);
+
+    a1 = ro * (b0 - a0) + a0;
+    b1 = b0 - ro * (b0 - a0);
 end
 
-[a0,b0]
+[a0, b0]
 
 function y = f(x)
-    y = x^4+4*x^3+9*x^2+6*x+6;
+    y = x^4 + 4 * x^3 + 9 * x^2 + 6 * x + 6;
 end
+
 
