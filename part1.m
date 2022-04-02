a8a5d7d0f2225248ea61eb8535426f510e1c8d88
unknown <thomas.dumond2001@gmail.com>
2022-04-02 18:45:35 +0200

    part1

--- part1.m
+++ part1.m
@@ -1,5 +1,15 @@
+A4 = [10 30 30 1 0 0; 1 1 1 0 1 0; 0 1 0 0 0 1];
+
+b4 = [2500 120 30]';
+
+c4 = [-474 -774 -645 0 0 0]';
+
+fprintf("\nAvec la méthode bourrin :\n");
+[f, x, B] = LP_bourrin(A4, b4, c4)
+
 function [f, x, B] = LP_bourrin(A, b, c)
 
+    % Check for errors in the arguments
     if ~ismatrix(A) || ~ismatrix(b) || ~ismatrix(c)
         error('Not matrix or vector');
     end
@@ -14,11 +24,26 @@ function [f, x, B] = LP_bourrin(A, b, c)
         error('b and c dont have same dimensions as A');
     end
 
-    for r1 = 1:m
-        for r2 = 1:n
-            B = [A(r1,1:n) A(r2,1:n)];
-            Binv = inv(B);
-            xb=Binv*b;
-            
-        end
+    % Initialisation of the variables
+    x = []; % x is the solution
+    B = []; % B is the basis
+    f = Inf; % f is the minimal cost
+    L = nchoosek(1:n, m); % L is the list of all the possible combinations of n elements
+
+    for i = 1:nchoosek(n, m)
+        % We choose the i-th combination of n elements
+        % We create the matrix B
+        B = A(:, L(i, :));
+        % We create the vector b
+        b = b(L(i, :));
+        % We create the vector c
+        c = c(L(i, :));
+        % We create the vector x
+        x = B \ b;
+        % We create the cost function
+        f = c' * x;
     end
+
+end
+
+   
